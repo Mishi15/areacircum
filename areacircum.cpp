b5072abbbb5320ba37e82994b1d4d5f0a76bd05b
Mishi15 <122174407+Mishi15@users.noreply.github.com>
2023-05-23 00:09:30 +0530

    Create areacircum.cpp

---
+++ areacircum.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+float r,area ,circum ;
+cout << "enter radius of circle";
+cin >> r ;
+area = 3.14 * (r *r );
+cout << area ;
+cout << "For circumference of circle";
+cin >> r ;
+circum = 2 * 3.14* r  ;
+cout << circum ;
+
+
+}
